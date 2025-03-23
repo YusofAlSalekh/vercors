d7af2c3ed3f82da063362ee1eae6f58e09e25395
YusofAlSalekh <y.alsalekh@gmail.com>
2025-03-23 19:09:39 +0100

    secondCommit

--- src/lsp/src/MyLanguageServerLauncher.scala
+++ src/lsp/src/MyLanguageServerLauncher.scala
@@ -3,10 +3,10 @@ package lsp
 import org.eclipse.lsp4j.launch.LSPLauncher
 
 object MyLanguageServerLauncher {
-  def main(args: Array[String]): Unit = {
-    val server = new MyLanguageServer()
-    val launcher = LSPLauncher.createServerLauncher(server, System.in, System.out)
+def main(args: Array[String]): Unit = {
+val server = new MyLanguageServer()
+val launcher = LSPLauncher.createServerLauncher(server, System.in, System.out)
     server.setClient(launcher.getRemoteProxy)
     launcher.startListening()
   }
-}
+          }
