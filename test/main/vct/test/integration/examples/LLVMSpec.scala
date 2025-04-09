d325fef06d8a673bd91493218e3f3188964c7ff4
Robert <27515600+RobertMensing@users.noreply.github.com>
2025-04-09 14:34:49 +0200

    Pallas: Assed swift-based test for assuem/ assert

--- test/main/vct/test/integration/examples/LLVMSpec.scala
+++ test/main/vct/test/integration/examples/LLVMSpec.scala
@@ -50,6 +50,7 @@ class LLVMSpec extends VercorsSpec {
   vercors should verify using silicon example "concepts/llvm/pallas/pallas_loop_goto.ll"
   vercors should fail withCode "assertFailed:false" using silicon example "concepts/llvm/pallas/pallas_c_assert_fail.ll"
   vercors should verify using silicon example "concepts/llvm/pallas/pallas_c_assert.ll"
+  vercors should verify using silicon example "concepts/llvm/pallas/pallas_swift_assert.ll"
 
   // Swift
   vercors should verify using silicon flags("--pallas-sroa") example "concepts/llvm/pallas/pallas_swift_fib.ll"
--- test/main/vct/test/integration/helper/ExampleFiles.scala
+++ test/main/vct/test/integration/helper/ExampleFiles.scala
@@ -52,6 +52,7 @@ case object ExampleFiles {
     "examples/concepts/llvm/pallas/pallas_loop_goto.c",
     "examples/concepts/llvm/pallas/pallas_c_assert_fail.c",
     "examples/concepts/llvm/pallas/pallas_c_assert.c",
+    "examples/concepts/llvm/pallas/pallas_swift_assert.swift",
   ).map(_.replaceAll("/", File.separator))
 
   val EXCLUSIONS: Seq[Path => Boolean] = Seq(
